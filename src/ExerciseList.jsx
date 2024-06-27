d4951cd797c13a510080b3be7cddcf136a19ca48
Leo St. Amour <lstamour@vt.edu>
2024-06-27 14:43:21 -0400

    Updates site text; configurable site title and root URL

--- src/ExerciseList.jsx
+++ src/ExerciseList.jsx
@@ -24,58 +24,69 @@ function ExerciseList() {
 
     return (
         <Container>
-            <h1>Practice Safety and Security Debugging</h1>
+            <h1>{process.env.REACT_APP_TITLE}</h1>
             <Row>
                 <Col>
                     <p>
-                        Welcome to TBD.
-                        This site is designed to provide you with small debugging exercises.
-                        Please select an exercise from the list below.
-                        Each exercise is categorized according to a common weakness enumeration (CWE).
+                        Welcome to {process.env.REACT_APP_TITLE}.
+                        This site is designed to provide you with small debugging exercises that focus on safety/security-related bugs.
+                        The exercises are categorized according to the common weakness enumeration (CWE) that they train.
+                        Additionally, each exercise is tagged as either <Badge bg="danger" pill>offensive</Badge> or <Badge bg="primary" pill>defensive</Badge>.
+                        The offensive exercises focus on crafting inputs that exploit or take advantage of the vulnerabilities whereas the defensive ones aim to patch or eliminate the bugs.
+                        For your convenience, each category contains links to additional training materials or resources.
                     </p>
+                    <p>
+                        Please select from the list of available exercises below.
+                        We request that you attempt both an offensive and defensive exercise from at least one of the categories.
+                        However, if you feel so inclined, please feel free to attempt any or all of the others.
+                    </p>
+                </Col>
+            </Row>
+            <Row>
+                <Col></Col>
+                <Col xs={8}>
+                    <h3>Available Exercises</h3>
+                    {Object.entries(exercises).map(([category, exercises]) => (
+                        <>
+                            <Row>
+                                <Col><h4>{category}</h4></Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <ListGroup>
+                                        {exercises.filter(exercise => exercise.type === 'resource').map((exercise) => (
+                                            <ListGroup.Item className="d-flex justify-content-between align-items-start">
+                                                <div className="ms-2 me-auto">
+                                                    <Link to={exercise.url} target="_blank">{exercise.title}</Link>
+                                                </div>
+                                                <Badge bg="success" pill>resource</Badge>
+                                            </ListGroup.Item>
+                                        ))}
+                                        {exercises.filter(exercise => exercise.type === 'offensive').map((exercise) => (
+                                            <ListGroup.Item className="d-flex justify-content-between align-items-start">
+                                                <div className="ms-2 me-auto">
+                                                    <Link to={`/exercise/${exercise.id}`}>{exercise.title}</Link>
+                                                </div>
+                                                <Badge bg="danger" pill>offensive</Badge>
+                                            </ListGroup.Item>
+                                        ))}
+                                        {exercises.filter(exercise => exercise.type === 'defensive').map((exercise) => (
+                                            <ListGroup.Item className="d-flex justify-content-between align-items-start">
+                                                <div className="ms-2 me-auto">
+                                                    <Link to={`/exercise/${exercise.id}`}>{exercise.title}</Link>
+                                                </div>
+                                                <Badge bg="primary" pill>defensive</Badge>
+                                            </ListGroup.Item>
+                                        ))}
+                                    </ListGroup>
+                                </Col>
+                            </Row>
+                            <br></br>
+                        </>
+                    ))}
                 </Col>
                 <Col></Col>
             </Row>
-            <h3>Available Exercises</h3>
-            {Object.entries(exercises).map(([category, exercises]) => (
-                <>
-                    <Row>
-                        <Col><h4>{category}</h4></Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <ListGroup>
-                                {exercises.filter(exercise => exercise.type === 'resource').map((exercise) => (
-                                    <ListGroup.Item className="d-flex justify-content-between align-items-start">
-                                        <div className="ms-2 me-auto">
-                                            <Link to={exercise.url} target="_blank">{exercise.title}</Link>
-                                        </div>
-                                        <Badge bg="success" pill>resource</Badge>
-                                    </ListGroup.Item>
-                                ))}
-                                {exercises.filter(exercise => exercise.type === 'offensive').map((exercise) => (
-                                    <ListGroup.Item className="d-flex justify-content-between align-items-start">
-                                        <div className="ms-2 me-auto">
-                                            <Link to={`/exercise/${exercise.id}`}>{exercise.title}</Link>
-                                        </div>
-                                        <Badge bg="danger" pill>offensive</Badge>
-                                    </ListGroup.Item>
-                                ))}
-                                {exercises.filter(exercise => exercise.type === 'defensive').map((exercise) => (
-                                    <ListGroup.Item className="d-flex justify-content-between align-items-start">
-                                        <div className="ms-2 me-auto">
-                                            <Link to={`/exercise/${exercise.id}`}>{exercise.title}</Link>
-                                        </div>
-                                        <Badge bg="primary" pill>defensive</Badge>
-                                    </ListGroup.Item>
-                                ))}
-                            </ListGroup>
-                        </Col>
-                        <Col></Col>
-                    </Row>
-                    <br></br>
-                </>
-            ))}
         </Container >
     );
 }
